e56e870ad51459b2094288ca23d012759157d693
No767 <73260931+No767@users.noreply.github.com>
2023-09-08 14:50:08 -0700

    [skip ci] Hopefully fix docs?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,7 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("../discord/ext/ipcx"))
 sys.path.insert(0, os.path.abspath("discord/ext/ipcx"))
 
