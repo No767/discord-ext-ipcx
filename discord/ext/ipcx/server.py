cc6553eef0d02597426c00e7eee585afc41d35d4
Noelle Wang <73260931+No767@users.noreply.github.com>
2026-05-17 20:23:31 -0700

    Adopt ty for type checking (#77)

--- discord/ext/ipcx/server.py
+++ discord/ext/ipcx/server.py
@@ -275,14 +275,14 @@ class Server:
         self.bot.dispatch("ipc_ready")
 
         self._server = aiohttp.web.Application()
-        self._server.router.add_route("GET", "/", self.handle_accept)  # type: ignore
+        self._server.router.add_route("GET", "/", self.handle_accept)
 
         if self.do_multicast:
             self._multicast_server = aiohttp.web.Application()
             self._multicast_server.router.add_route(
                 "GET",
                 "/",
-                self.handle_multicast,  # type: ignore
+                self.handle_multicast,
             )
 
             await self._start(self._multicast_server, self.multicast_port)
